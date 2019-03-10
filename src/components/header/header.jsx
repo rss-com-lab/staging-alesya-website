f2108bb5ae88d0a5ce9291d821d65f42116e8934
AntonyKor <antonykor@gmail.com>
2019-03-10 15:47:10 +0300

    feat: add header styling

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,29 +3,35 @@ import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
 import LocalizedLink from '../localized-link';
+import styles from './header.module.scss';
 
 const Header = ({ parentStyles = {} }) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation('header');
 
   return (
-    <header
-      style={{
-        background: `rebeccapurple`,
-        marginBottom: `1.45rem`,
-      }}
-    >
+    <header className={styles.header}>
       <div className={parentStyles.container}>
-        <h1 style={{ margin: 0 }}>
-          <LocalizedLink
-            to="/"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
-          >
-            {t('test')}
+        <h2 style={{ margin: 0 }}>
+          <LocalizedLink to="/">
+            {t('title')}
           </LocalizedLink>
-        </h1>
+        </h2>
+        <nav>
+          <ul>
+            <li>
+              <LocalizedLink to="/" activeClassName={styles.active}>{t('About me')}</LocalizedLink>
+            </li>
+            <li>
+              <LocalizedLink to="/writing" activeClassName={styles.active}>{t('Writing')}</LocalizedLink>
+            </li>
+            <li>
+              <LocalizedLink to="/blog" activeClassName={styles.active}>{t('Blog')}</LocalizedLink>
+            </li>
+            <li>
+              <LocalizedLink to="/contact" activeClassName={styles.active}>{t('Contact')}</LocalizedLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
