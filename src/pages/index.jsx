b7fb729a8a6626afca9dcdcda9b5ac3a237bbf84
AntonyKor <antonykor@gmail.com>
2019-03-10 16:11:47 +0300

    fix: delete info from index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,20 +2,12 @@ import React from "react";
 import { withTranslation } from 'react-i18next';
 
 import Layout from "../components/layout/layout";
-import Image from "../components/image";
-import LocalizedLink from '../components/localized-link';
 
 const IndexPage = ({ pathContext, t, i18n }) => {
   if (i18n.language !== pathContext.locale) i18n.changeLanguage(pathContext.locale);
   return (
     <Layout path={pathContext.pathname}>
       <h1>{t('Hi people')}</h1>
-      <p>{t('Welcome to your new Gatsby site')}</p>
-      <p>{t('Now go build something great')}</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <LocalizedLink to="/page-2/">{t('Go to page')} 2</LocalizedLink>
     </Layout>
   )
 }
